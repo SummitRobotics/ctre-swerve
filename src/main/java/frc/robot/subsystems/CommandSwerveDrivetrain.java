a85d4362f5c858aac2b80b9918168dbc3e5dddb1
driverstation <>
2025-01-13 16:41:15 -0800

    Turn and odometry tuning

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -11,6 +11,8 @@ import com.ctre.phoenix6.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
@@ -18,10 +20,12 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-
+import frc.robot.LimelightHelpers;
+import frc.robot.LimelightHelpers.LimelightResults;
 import frc.robot.generated.TunerConstants.TunerSwerveDrivetrain;
 
 /**
@@ -233,6 +237,28 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
                 );
                 m_hasAppliedOperatorPerspective = true;
             });
+        }
+        // First, tell Limelight your robot's current orientation
+        // double robotYaw = getRotation3d().getAngle();
+        // LimelightHelpers.SetRobotOrientation("limelight-tags", robotYaw, 0.0, 0.0, 0.0, 0.0, 0.0);
+
+        // Get the pose estimate
+        // LimelightHelpers.PoseEstimate limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("tags");
+        LimelightResults llResults = LimelightHelpers.getLatestResults("limelight-tags");
+        System.out.println("Tags: " + llResults.targets_Fiducials.length);
+        Pose2d botPose = llResults.getBotPose2d();
+
+        // Add it to your pose estimator
+        if (llResults.valid && botPose.getX() != 0 && botPose.getY() != 0) {
+            // System.out.println("Used vision estimate");
+            setVisionMeasurementStdDevs(VecBuilder.fill(.0005, .0005, 0.01));
+            addVisionMeasurement(
+                botPose,
+                Timer.getFPGATimestamp() - llResults.latency_pipeline / 1000.0
+                    - llResults.latency_capture / 1000.0
+            );
+        } else if (!llResults.valid) {
+            // System.out.println("Rejected limelight data");
         }
     }
 
